bb6e3a2671cee77673740b2ee092b2935d970ce1
Riccarda Ries <r.ries@awesome-software.de>
2021-12-17 21:19:34 +0100

    move logo to separate component

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,15 +4,21 @@ interface HeaderProps {
   isLoggedIn: boolean;
 }
 
+export const Logo: FC = (): JSX.Element => {
+  return (
+    <div className="logo">
+      <div className="logo__tick-mark" />
+      <div className="logo__name">
+        any<span className="logo__name--bold">task</span>
+      </div>
+    </div>
+  );
+};
+
 const Header: FC<HeaderProps> = (): JSX.Element => {
   return (
     <div className="header">
-      <div className="header__logo">
-        <div className="header__tick-mark" />
-        <div className="header__name">
-          any<span className="header__name--bold">task</span>
-        </div>
-      </div>
+      <Logo />
     </div>
   );
 };
