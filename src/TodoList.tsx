d4772da263f254c9647588db63329faa5f7f8980
Riccarda Ries <r.ries@awesome-software.de>
2021-12-19 12:49:46 +0100

    add list header

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,11 +1,15 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import TodoListItem from './TodoListItem';
 import RoundButton from './RoundButton';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTasks } from './redux/taskSlice';
 import { RootState } from './redux/store';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons';
+import Button from './Button';
 
 const TodoList: FC = (): JSX.Element => {
+  const [selectMode, setSelectMode] = useState<boolean>(false);
   const dispatch = useDispatch();
   const tasks = useSelector((state: RootState) => state.tasks.tasks);
 
@@ -27,8 +31,25 @@ const TodoList: FC = (): JSX.Element => {
     dispatch(fetchTasks());
   };
 
+  const handleSelectUnselect = () => {
+    setSelectMode(!selectMode);
+  };
+
+  const buttonText = () => (selectMode ? 'Abbrechen' : 'Auswählen');
+
   return (
     <div className="todoList">
+      <div className="todoList__header">
+        <div className="todoList__header-content">
+          <div className="todoList__back-action">
+            <div className="todoList__back-btn">
+              <FontAwesomeIcon icon={faLongArrowAltLeft} />
+              <span>Zurück</span>
+            </div>
+          </div>
+          <Button buttonText={buttonText()} onClick={handleSelectUnselect} />
+        </div>
+      </div>
       <div className="todoList__content">
         <div className="todoList__headline">Ihre heutigen Tasks:</div>
         {tasks &&
